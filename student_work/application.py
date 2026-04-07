7eed83d98be6c41b71e35d6e5a5c7ad60b0bc3c0
DylanCook09 <s1073734@student.knoxschools.org>
2026-04-07 16:01:58 +0000

    now bigger title

--- student_work/application.py
+++ student_work/application.py
@@ -158,7 +158,7 @@ register_page = f"""{base_style}
 
 secret_page = f"""{secondary_style}
 <div class="card">
-<h9>Nutriplanr </h9>
+<h1>Nutriplanr </h1>
 <h3>Welcome, {{{{ username }}}}!</h3>
 <p>Lets get fit!</p>
 <a href="/logout"><button>Logout</button></a>
