f5a28b8fa1d6c75b8eae0febc3c0e4647dcd3783
Eli-Ba <s1006974@student.knoxschools.org>
2026-04-07 15:47:45 +0000

    style changes

--- student_work/application.py
+++ student_work/application.py
@@ -75,8 +75,8 @@ body {
     padding: 25px;
     border-radius: 10px;
     box-shadow: 0 4px 10px rgba(0,0,0,0.1);
-    width: 600px;
-    height: 500px;
+    width: 800px;
+    height: 700px;
     text-align: center;
 }
 input {
