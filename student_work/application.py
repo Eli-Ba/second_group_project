d18282653b3efbbd2099c0f8f3189bbaced0922e
Eli-Ba <s1006974@student.knoxschools.org>
2026-04-07 15:55:25 +0000

    full pixles

--- student_work/application.py
+++ student_work/application.py
@@ -75,8 +75,8 @@ body {
     padding: 25px;
     border-radius: 10px;
     box-shadow: 0 4px 10px rgba(0,0,0,0.1);
-    width: 800px;
-    height: 700px;
+    width: 1500px;
+    height: 1400px;
     text-align: center;
 }
 input {
